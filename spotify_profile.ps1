cc9080d6c00f5617126bdedab61c16663dd3ac04
jgarza9788 <jgarza9788@gmail.com>
2022-05-30 12:31:01 -0400

    ...

---
+++ spotify_profile.ps1
@@ -0,0 +1,16 @@
+
+# $wshell = New-Object -ComObject wscript.shell
+
+# $wshell.AppActivate('edge')
+
+# Install-Module Focus-Window
+# Focus-Window 'wt'
+
+# taskkill /IM spotify.exe /f
+start spotify 
+# Start-Sleep -Seconds 1.0
+# $wshell.SendKeys('%{TAB}')
+
+# C:\Users\JGarza\misc_programs\spt.exe -play
+
+spt -play 
--- user_profile.ps1
+++ user_profile.ps1
@@ -1,12 +1,12 @@
 #https://github.com/craftzdog/dotfiles-public/blob/master/.config/powershell/user_profile.ps1
 
 #mh-my-posh theme
-# $theme_config = Join-Path $env:USERPROFILE ".\AppData\Local\Programs\oh-my-posh\themes\darkblood.omp.json"
+
 $theme_config = Join-Path $env:USERPROFILE ".config\powershell\custom.omp.json"
 echo $theme_config
 oh-my-posh --init --shell pwsh --config $theme_config | Invoke-Expression
-# oh-my-posh init pwsh --config ~/.cert.omp.json | Invoke-Expression
 
+# icons in terminal
 Import-Module -Name Terminal-Icons
 
 # PSReadLine
@@ -19,6 +19,21 @@ Set-PSReadLineOption -PredictionSource History
 Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
+# spotify function
+Function spotify-thing
+{
+    start spotify 
+    # Start-Process spotify -WindowStyle Minimized
+    # Start-Sleep -Seconds 2.0
+
+    # Add-Type -AssemblyName System.Windows.Forms
+    # [System.Windows.Forms.SendKeys]::SendWait("%{TAB}{ENTER}")
+
+    # (Get-Process | Where-Object {$_.name -Match 'Terminal'}) | Set-WindowState -State MINIMIZE
+    # Start-Process spotify -WindowStyle Minimized
+    C:\Users\JGarza\misc_programs\spt.exe
+}
+
 # Alias
 Set-Alias run start
 Set-Alias open start
@@ -27,16 +42,20 @@ Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+Set-Alias -Name spt -Value spotify-thing -Description "runs spotify"
+
+# hide dot folders in home folder
+# ATTRIB +H /d C:\User\JGarza\.*
 
 
 # conda + powershell
 # powershell -ExecutionPolicy ByPass -NoExit -Command "& 'C:\Users\JGarza\miniconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\Users\JGarza\miniconda3\'"
-conda init powershell -q
+# conda init powershell -q
 
 #  cd to folder
-cd .\github\
+cd C:\users\JGarza\github\
 
 # clear before showing
-clear
+# clear
 
 
